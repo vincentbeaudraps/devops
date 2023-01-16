0d293a8bb8c2294468d142db07d9c4c6cd45337d
Vincent <vincentbeaudraps@gmail.com>
2023-01-16 14:57:14 +0100

    commit 16/01

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 
 const app = express()
 
-app.get('/hello', (req, res) => res.send('Hello Simplon 1 test'));
+
+app.get('/hello', (req, res) => res.send(process.env.chaine));
 
 app.listen(3018, ()=> console.log("Application démarrée sur le port 3011"));
